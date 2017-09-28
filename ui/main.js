ce8dcb785f89a375f884572864929de5a389bb9e
priya134 <31379795+priya134@users.noreply.github.com>
2017-09-29 00:47:25 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,6 +2,9 @@ console.log('Loaded!');
 var element=document.getElementById('main-text');
 element.innerHTML='new value';
 var img=document.getElementById('madi');
-img.onclick=function(){
-    img.style.marginleft='100px';
-};
+var marginleft=0;
+function moverght(){
+    marginleft=marginleft+10;
+    img.stle.marginleft=marginleft+'px';
+    var interval=setInterval(moveright,100);
+}
